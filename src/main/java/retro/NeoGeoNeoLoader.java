1b0792f1682658b5b31439d48aaf317e13f745fa
hippietrail <hippytrail@gmail.com>
2024-11-01 16:20:58 +0700

    minimal loader for neo geo .neo format, can identify but not yet load

---
+++ src/main/java/retro/NeoGeoNeoLoader.java
@@ -0,0 +1,92 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package retro;
+
+import java.io.IOException;
+import java.util.*;
+
+import ghidra.app.util.Option;
+import ghidra.app.util.bin.BinaryReader;
+import ghidra.app.util.bin.ByteProvider;
+import ghidra.app.util.importer.MessageLog;
+import ghidra.app.util.opinion.AbstractProgramWrapperLoader;
+import ghidra.app.util.opinion.LoadSpec;
+import ghidra.framework.model.DomainObject;
+import ghidra.program.model.lang.LanguageCompilerSpecPair;
+import ghidra.program.model.listing.Program;
+import ghidra.util.exception.CancelledException;
+import ghidra.util.task.TaskMonitor;
+
+/**
+ * A {@link Loader} for loading Neo Geo .neo files.
+ */
+public class NeoGeoNeoLoader extends AbstractProgramWrapperLoader {
+
+    public static final String NEO_NAME = "Neo Geo .neo";
+    public static final String NEO_MAGIC = "NEO\1";
+    public static final int NEO_HEADER_LEN = 0x5e;
+
+	@Override
+	public String getName() {
+		return NEO_NAME;
+	}
+
+	@Override
+	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
+		List<LoadSpec> loadSpecs = new ArrayList<>();
+
+		if (provider.length() < NEO_HEADER_LEN) return loadSpecs;
+
+        BinaryReader reader = new BinaryReader(provider, false);
+
+        String magic = reader.readAsciiString(0, NEO_MAGIC.length());
+        if (!magic.equals(NEO_MAGIC)) return loadSpecs;
+
+        // Only ever used plain 68000 and didn't use A-line or F-line traps
+        loadSpecs.add(new LoadSpec(this, 0, new LanguageCompilerSpecPair("68000:BE:32:default", "default"), true));
+
+		return loadSpecs;
+	}
+
+	@Override
+	protected void load(ByteProvider provider, LoadSpec loadSpec, List<Option> options,
+			Program program, TaskMonitor monitor, MessageLog log)
+			throws CancelledException, IOException {
+
+		// TODO: Load the bytes from 'provider' into the 'program'.
+	}
+
+	@Override
+	public List<Option> getDefaultOptions(ByteProvider provider, LoadSpec loadSpec,
+			DomainObject domainObject, boolean isLoadIntoProgram) {
+		List<Option> list =
+			super.getDefaultOptions(provider, loadSpec, domainObject, isLoadIntoProgram);
+
+		// TODO: If this loader has custom options, add them to 'list'
+		list.add(new Option("Option name goes here", "Default option value goes here"));
+
+		return list;
+	}
+
+	@Override
+	public String validateOptions(ByteProvider provider, LoadSpec loadSpec, List<Option> options, Program program) {
+
+		// TODO: If this loader has custom options, validate them here.  Not all options require
+		// validation.
+
+		return super.validateOptions(provider, loadSpec, options, program);
+	}
+}
